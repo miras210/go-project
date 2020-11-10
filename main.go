d6285cbdfac5f540c9d43423b04cdf13b3f8e52f
Miras <sektor.miras@gmail.com>
2020-11-10 19:57:25 +0600

    added: prerequisites for the start of the game

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"fmt"
+	g "golang-project/game-package"
+)
 
 func main() {
-
+	fmt.Println("Welcome to the club buddie :)")
+	fmt.Println("My name is Van Darkholme. What is your name my sweetie ?)")
+	var playerName string
+	_, _ = fmt.Scanln(&playerName)
+	fmt.Printf("Wow, that's beautiful name, my %v\n", playerName)
+	var difficulty string
+	fmt.Println("If you come here, do you wanna play hard with me ? Choose [easy, medium, hard]")
+	_, _ = fmt.Scanln(&difficulty)
+	myGame := g.NewGame(playerName, difficulty)
+	myGame.StartGame()
 }
